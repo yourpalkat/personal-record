04df1962b9508734d2e894b6a9ccfb33c3f150b0
derekmurr <derekmurr@gmail.com>
2020-02-25 15:34:45 -0500

    List sorting and styling

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
@@ -9,8 +9,9 @@ import dashStyles from './Dashboard.module.scss';
 import 'react-big-calendar/lib/sass/styles.scss';
 import './CalendarEventStyles.scss';
 
-const Dashboard = ({ user, userRuns, setRun, selectedRun, location }) => {
+const Dashboard = ({ user, userRuns, setRun, selectedRun }) => {
   const localizer = momentLocalizer(moment);
+  const [viewRun, setViewRun] = useState(false);
 
   const selectRun = (run) => {
     setRun(run);
@@ -18,13 +19,13 @@ const Dashboard = ({ user, userRuns, setRun, selectedRun, location }) => {
 
   return(
     <>
-      {selectedRun._id ? <Redirect to={`/users/${user._id}/runs/${selectedRun._id}`} /> : (
+      {selectedRun._id && viewRun ? <Redirect to={`/users/${user._id}/runs/${selectedRun._id}`} /> : (
         <section className={dashStyles.dashboard}>
           <div className={dashStyles.titleBlock}>
             <h2>{user.firstName}’s workouts &emsp;</h2>
             <Button 
               buttonType='link'
-              linkPath={`${location}/runs/add`}
+              linkPath={`/users/${user._id}/runs/add`}
               buttonStyle='confirm'
               text='Add New Run' />
           </div>
@@ -36,7 +37,9 @@ const Dashboard = ({ user, userRuns, setRun, selectedRun, location }) => {
               defaultView='month' 
               views={['month', 'week']} 
               defaultDate={new Date()} 
-              onSelectEvent={(event) => selectRun(event)}
+              onSelectEvent={(event) => {
+                selectRun(event);
+                setViewRun(true);}}
               eventPropGetter={(event) => { return { className: `${event.workoutType}Run`} }}
             />
           </div>
