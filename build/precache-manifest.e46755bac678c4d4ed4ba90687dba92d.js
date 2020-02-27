2bfe43ed827cadedd709d43be3c6b5c43706e050
derekmurr <derekmurr@gmail.com>
2020-02-27 15:19:50 -0500

    Tweaks to server to prepare for deploy

---
+++ build/precache-manifest.e46755bac678c4d4ed4ba90687dba92d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865ceca4c556c525f3c3b4d5b809b755",
+    "url": "/derekmurr/personal-record/index.html"
+  },
+  {
+    "revision": "eeeeb038dc9dfcf995b4",
+    "url": "/derekmurr/personal-record/static/css/2.93f1c839.chunk.css"
+  },
+  {
+    "revision": "76d8ad20bc7c0e4ab458",
+    "url": "/derekmurr/personal-record/static/css/main.152388d4.chunk.css"
+  },
+  {
+    "revision": "eeeeb038dc9dfcf995b4",
+    "url": "/derekmurr/personal-record/static/js/2.f2610bc7.chunk.js"
+  },
+  {
+    "revision": "76d8ad20bc7c0e4ab458",
+    "url": "/derekmurr/personal-record/static/js/main.32a09d67.chunk.js"
+  },
+  {
+    "revision": "a8767262117d49069731",
+    "url": "/derekmurr/personal-record/static/js/runtime~main.eeb8af5c.js"
+  },
+  {
+    "revision": "d6fc5ec5403ec65bdaecfe44b7b84402",
+    "url": "/derekmurr/personal-record/static/media/RobotoCondensed-Italic.d6fc5ec5.ttf"
+  },
+  {
+    "revision": "52ee8b598488b1ffbaa93e50cbd6a2f4",
+    "url": "/derekmurr/personal-record/static/media/RobotoCondensed-Regular.52ee8b59.ttf"
+  },
+  {
+    "revision": "0b2aeb1c9f580b22533476443b47f0ad",
+    "url": "/derekmurr/personal-record/static/media/RobotoSlab-VariableFont.0b2aeb1c.ttf"
+  },
+  {
+    "revision": "ce750d4a85c6d2b6b5f8013e87790f71",
+    "url": "/derekmurr/personal-record/static/media/arrow-down-solid.ce750d4a.svg"
+  },
+  {
+    "revision": "960adab179cfd2d1b2f6d8e2cf9c68ef",
+    "url": "/derekmurr/personal-record/static/media/arrow-up-solid.960adab1.svg"
+  },
+  {
+    "revision": "3b05929c9800109ddc496cc5ab9e323a",
+    "url": "/derekmurr/personal-record/static/media/chevron-down-solid.3b05929c.svg"
+  },
+  {
+    "revision": "e164ccff2f012eb68ffd4fbc308ec411",
+    "url": "/derekmurr/personal-record/static/media/splash.e164ccff.jpg"
+  }
+]);
